3a85a1e93206ca9a325de4241b4c27755ac7bd68
doberman7 <ivanrubioangeles@gmail.com>
2021-04-18 18:15:18 -0500

    eslint1

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,45 @@
-import Head from 'next/head'
-import {useEffect} from 'react'
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function Home(initialData) {
-  useEffect(()=>{
-    console.log(initialData);
-  })
-  //Now that you know that the onChange event is working to grab the input’s value, you will create a function to get the value of inputs inside your component. Note that the component is also a function. 
-  const handleInputs = (event) => {
-    conslole.log(event.target.value)
-    conslole.log(event.target.name)
-  }
-  return (
-    <div className='container'>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="/styles.css"/>
-      </Head>
+    useEffect(() => {
+        console.log(initialData);
+    });
+    //Now that you know that the onChange event is working to grab the input’s value, you will create a function to get the value of inputs inside your component. Note that the component is also a function.
+    const handleInputs = (event) => {
+        conslole.log(event.target.value);
+        conslole.log(event.target.name);
+    };
+    return (
+        <div className="container">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="/styles.css" />
+            </Head>
 
-      <h1>Giphy Search App</h1>
-      <form>
-           <input name="searchTerm" onChange={() => console.log('happens')} type="text"/>
-      </form>
-      <div className="giphy-search-results-grid">
-        {initialData.catGiphys.data.map((each, index) => {
-          return(
-            <div key={index}>
-              <h3>{each.title}</h3>
-            <img src={each.images.original.url} alt={each.title}/>
+            <h1>Giphy Search App</h1>
+            <form>
+                <input name="searchTerm" onChange={() => console.log('happens')} type="text" />
+            </form>
+            <div className="giphy-search-results-grid">
+                {initialData.catGiphys.data.map((each, index) => {
+                    return (
+                        <div key={index}>
+                            <h3>{each.title}</h3>
+                            <img src={each.images.original.url} alt={each.title} />
+                        </div>
+                    );
+                })}
             </div>
-          )
-        })}
-      </div>
-    </div>
-  )
+        </div>
+    );
 }
 
 export async function getStaticProps() {
-  let catGiphys = await fetch('https://api.giphy.com/v1/gifs/search?q=cats&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=8')
-  catGiphys = await catGiphys.json()
-  return {props: {catGiphys: catGiphys}}  
+    let catGiphys = await fetch(
+        'https://api.giphy.com/v1/gifs/search?q=cats&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=8'
+    );
+    catGiphys = await catGiphys.json();
+    return { props: { catGiphys: catGiphys } };
 }
