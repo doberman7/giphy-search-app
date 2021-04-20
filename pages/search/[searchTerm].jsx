b1bff55df0a3b2305a57cfef80aa834eeefc0479
doberman7 <ivanrubioangeles@gmail.com>
2021-04-19 20:39:03 -0500

    search page

---
+++ pages/search/[searchTerm].jsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+export default function Search(initialData) {
+    const router = useRouter();
+    return (
+        <>
+            <Head>
+                <title>Search</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="/styles.css" />
+            </Head>
+            <h1>Search results for: {router.query.searchTerm}</h1>
+
+            {/* You access the parameter with the name you assigned it. It will be located inside the
+            router.params object. */}
+            <h1>Search results for: {router.query.searchTerm}</h1>
+            <div className="giphy-search-results-grid">
+                {initialData.giphys.map((each, index) => {
+                    return (
+                        <div key={index}>
+                            <h3>{each.title}</h3>
+                            <img src={each.images.original.url} alt={each.title} />
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
+}
+export async function getServerSideProps(context) {
+    const searchTerm = context.query.searchTerm;
+    let giphys = await fetch(
+        `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6`
+    );
+    giphys = await giphys.json();
+    return { props: { giphys: giphys.data } };
+}
