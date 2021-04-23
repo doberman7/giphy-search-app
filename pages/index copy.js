808589d002027ac9acf5e7e55d98b1059bc0890d
doberman7 <ivanrubioangeles@gmail.com>
2021-04-22 21:02:42 -0500

    image opt defectuous

--- pages/index copy.js
+++ pages/index copy.js
@@ -1,8 +1,9 @@
 import Head from 'next/head';
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Footer from '../components/Footer';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import Footer from '../components/Footer';
+
 export default function Home(initialData) {
     const [formInputs, setFormInputs] = useState();
     const [searchTerm, setSearchTerm] = useState('cats');
@@ -16,16 +17,17 @@ export default function Home(initialData) {
         let { name, value } = event.target;
         setFormInputs({ ...formInputs, [name]: value });
     };
+
     const search = async (event) => {
         event.preventDefault();
         let giphys = await fetch(
-            `https://api.giphy.com/v1/gifs/search?q=${formInputs.searchTerm}&api_key=${process.env.giphy_key}&limit=6`
+            `https://api.giphy.com/v1/gifs/search?q=${formInputs.searchTerm}&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6`
         );
         giphys = await giphys.json();
         setSearchResults(giphys.data);
         setSearchTerm(formInputs.searchTerm);
     };
-    console.log(searchResults);
+
     return (
         <>
             <div className="container">
@@ -33,26 +35,31 @@ export default function Home(initialData) {
                     <title>Giphy Search App</title>
                     <meta
                         name="description"
-                        content="Love giphys? We do too. Use our advanced giphy search to find the perfect giphy for any occation"></meta>
-
+                        content="Love giphys? We do too. Use our advanced giphy search to find the perfect giphy for any occasion"></meta>
                     <link rel="icon" href="/favicon.ico" />
                     <link rel="stylesheet" href="/styles.css" />
                 </Head>
 
                 <div className="logo-container">
-                    <Image src="/logo.png" alt="logo" width="100px" height="100px" />
+                    <Image src="/logo.png" alt="logo" unsized />
                 </div>
+
                 <form onSubmit={search}>
                     <input name="searchTerm" onChange={handleInputs} type="text" required />
                     <button>Search</button>
                 </form>
+
                 <h1>Search results for: {searchTerm}</h1>
-                <Link href="/search/[pid]" as={`/search/${searchTerm}`}>
-                    <a>{`http://localhost:3000/search/${searchTerm}`}</a>
-                </Link>
-                <h1>Giphy Search App</h1>
+
+                <p>
+                    Share this search with others:
+                    <Link href="/search/[pid]" as={`/search/${searchTerm}`}>
+                        <a>{`http://localhost:3000/search/${searchTerm}`}</a>
+                    </Link>
+                </p>
+
                 <div className="giphy-search-results-grid">
-                    {searchResults?.map((each, index) => {
+                    {searchResults.map((each, index) => {
                         return (
                             <div key={index}>
                                 <h3>{each.title}</h3>
@@ -69,7 +76,7 @@ export default function Home(initialData) {
 
 export async function getStaticProps() {
     let catGiphys = await fetch(
-        `https://api.giphy.com/v1/gifs/search?q=cats&api_key=${process.env.giphy_key}&limit=8`
+        'https://api.giphy.com/v1/gifs/search?q=cats&api_key=nPJNlVceWHERWCSDBW5XMo1p90l7l9ie&limit=6'
     );
     catGiphys = await catGiphys.json();
     return { props: { catGiphys: catGiphys } };
